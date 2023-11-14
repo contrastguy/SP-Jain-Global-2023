071377ae45d34cab55e9be8b0d550193db85416b
msilva7 <Matheus.Silva@fazenda.rj.gov.br>
2023-11-14 00:27:32 -0300

    Add: files

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{html,js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -13,6 +13,9 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    container: {
+      center: true,
+    }
   },
   plugins: [],
 }
