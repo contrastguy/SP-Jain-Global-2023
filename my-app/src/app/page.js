6cfccb8903c10a6e4a489da7d7032a2f700e7810
msilva7 <Matheus.Silva@fazenda.rj.gov.br>
2023-11-18 17:13:47 -0300

    add: files

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="mx-auto md:max-w-3xl">
@@ -23,33 +25,34 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="container mx-auto px-4 md:container md:mx-auto bg-orange-300 h-72 w-full justify-center rounded-md flex  ">
-          <div className="items-center">
-            <p>The universal destinations platform</p>
-            <p>
+        <div className="container mx-auto px-4 md:container md:mx-auto bg-orange-400 h-72 w-full justify-center rounded-md flex  ">
+          <div className="">
+            <p className="text-3xl p-2 font-semibold">The universal destinations platform</p>
+            <p className="p-2">
               Supporting students to <b>find the best next step</b> for them
             </p>
+            <button class="inline-flex items-center  rounded-md bg-gray-50 px-2 py-1  font-semibold text-black  ring-1 ring-inset ring-gray-500/10 ">Request a Demo</button>
           </div>
-          <div>
-            <img src="my-app\public\academic-png.png"></img>
+          <div className="p-4">
+            <Image src="/academic-png.png" width="124" height="124"/> 
           </div>
         </div>
 
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href=""
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-orange-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
-              Docs{" "}
+              Study Plan{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find in-depth information about Next.js features and API.
+              Create a custom study plan to organize your routine
             </p>
           </a>
 
@@ -60,18 +63,35 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>
-              Learn{" "}
+              Explore{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
+              Learn more about a Institute or University
             </p>
           </a>
 
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-orange-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Find{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              It's a Match ! Find the University that suits you best
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-orange-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
@@ -83,24 +103,7 @@ export default function Home() {
               </span>
             </h2>
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Explore the Next.js 13 playground.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-orange-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Deploy{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
+              We give you created Study Plans, based on different routines 
             </p>
           </a>
         </div>
